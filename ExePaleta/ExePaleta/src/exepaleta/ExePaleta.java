e95c6126f1748eabdd8070000735f07226bbcace
finfanterodal <finfanterodal@nautilus11.danielcastelao.org>
2019-05-06 10:07:58 +0200

    Commit inicial.

---
+++ ExePaleta/ExePaleta/src/exepaleta/ExePaleta.java
@@ -0,0 +1,17 @@
+package exepaleta;
+
+/**
+ *
+ * @author finfanterodal
+ */
+public class ExePaleta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana1 v1=new Ventana1();
+        v1.setVisible(true);
+    }
+
+}
