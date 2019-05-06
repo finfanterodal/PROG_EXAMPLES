5fddbe01b91e2886921b76585f87dfac1a03bfd7
finfanterodal <finfanterodal@nautilus11.danielcastelao.org>
2019-05-06 10:05:37 +0200

    Commit inicial.

---
+++ EnlaceNetbeans/src/enlacenetbeans/EnlaceNetbeans.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ 
+ * modifico.
+ */
+package enlacenetbeans;
+
+/**
+ *
+ * @author finfanterodal
+ */
+public class EnlaceNetbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //mi primer comentario
+        //segundo comentario
+    }
+    
+}
